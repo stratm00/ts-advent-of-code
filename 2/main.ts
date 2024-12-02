5260d0441ca078edf58de1eb5e5e685908016085
stratm00 <108333431+stratm00@users.noreply.github.com>
2024-12-02 11:11:27 +0100

    D2

---
+++ 2/main.ts
@@ -0,0 +1,50 @@
+import fs from "node:fs"; 
+
+type ReportData = number[]; 
+const ACCEPTABLE_DELTAS = [1,3];
+
+fs.readFile("./2/input.txt", (err, data)=>{
+    if(err){
+        console.error(err);
+        return;
+    }
+    let reports: ReportData[] = [];
+    const dataString = data.toString();
+
+    for (const reportString of dataString.split("\n")){
+        if(reportString!==""){
+            reports.push( reportString.split(" ").map((val)=>Number(val)) );
+        }
+    }
+
+    const numberOfSafeReports = reports.map((rep)=>reportSafe(rep)).filter(x=>x).length;
+
+    console.log(`Es gab ${numberOfSafeReports} sichere Reports.`)
+});
+
+function reportSafe(report:ReportData):boolean {
+    let monotonous_falling_action = true;
+    let monotonous_rising_action = true;
+    let action_in_acceptable_deltas = true;
+
+    //Naive solution
+    for(let index in report){
+        //JS Array indices are Strings, fun things happen when you use expressions like index+1
+        const num_index = Number(index)
+        if(num_index< report.length-1){
+            let delta_nums = report[num_index]-report[num_index+1];
+
+            if(delta_nums<0){
+                monotonous_falling_action = false;
+            }else if(delta_nums>0){
+                monotonous_rising_action = false;
+            }
+
+            const abs_delta = Math.abs(delta_nums)
+            action_in_acceptable_deltas = action_in_acceptable_deltas && ( (abs_delta>=ACCEPTABLE_DELTAS[0]) && (abs_delta<=ACCEPTABLE_DELTAS[1]));
+        }
+    }
+    
+    return ((monotonous_rising_action || monotonous_falling_action) && action_in_acceptable_deltas)
+    
+}
