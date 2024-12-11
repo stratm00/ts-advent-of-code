460bc0355c83287e630fdd152493ec3642ec6b48
stratm00 <108333431+stratm00@users.noreply.github.com>
2024-12-11 10:54:05 +0100

    D11 pt1 accomplished

---
+++ 11/main.ts
@@ -0,0 +1,34 @@
+import fs from 'node:fs';
+const ITERATIONS = 25;
+fs.readFile("./11/input.txt", (err, data)=> {
+   
+    if(err){
+        console.error(err);
+        return;
+    }
+    const inputListOfNumbers: number[] = data.toString().split(" ").map(Number);
+    console.log("initial reading:");
+    console.log(inputListOfNumbers.join(" "));
+
+    let latestIteration = inputListOfNumbers;
+    for(let i = 0;i<ITERATIONS; i++){
+        latestIteration = latestIteration.map(applyRules).reduce(arrUnion, []);
+    }
+
+    console.log(`Length of final Iteration result: ${latestIteration.length}`);
+});
+
+function applyRules(num: number): number[] {
+    if (num==0) return [1];
+    if (num.toString().length % 2 == 0){
+        const numString = num.toString();
+        return [numString.substring(0, numString.length/2), numString.substring(numString.length/2)].map(Number);
+    }
+    return [num * 2024];
+}
+function arrUnion<T> (a:T[], b:T[]):T[] {
+    
+    for(const item of b)a.push(item);
+    return a
+    ;
+}
---
+++ 11/main2.ts
@@ -0,0 +1,36 @@
+import fs from 'node:fs';
+const ITERATIONS = 25;
+
+fs.readFile("./11/input.txt", (err, data)=> {
+   
+    if(err){
+        console.error(err);
+        return;
+    }
+    const inputListOfNumbers: number[] = data.toString().split(" ").map(Number);
+
+    let latestIteration = inputListOfNumbers;
+    
+    for(let i = 0;i<ITERATIONS; i++){
+        latestIteration = latestIteration.map(applyRules).reduce(arrUnion, []);
+    }
+
+    
+});
+
+function applyRules(num: number): number[] {
+    if (num==0) return [1];
+    if (num.toString().length % 2 == 0){
+        const numString = num.toString();
+        return [numString.substring(0, numString.length/2), numString.substring(numString.length/2)].map(Number);
+    }
+    return [num * 2024];
+}
+
+
+
+function arrUnion<T> (a:T[], b:T[]):T[] {
+    
+    for(const item of b)a.push(item);
+    return a;
+}
